9bdae2f7ae7335cdbe31604e97120987fc858be5
Manuel <info@toolwheel.de>
2025-01-18 12:31:12 +0100

    Fehler in der WinForm Vorlage behoben.

--- ToolWheel.WinFormApp.Template/src/Program.cs
+++ ToolWheel.WinFormApp.Template/src/Program.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using 
 
 internal class Program
 {
     public static void Main()
     {
-
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
     }
 }
 
